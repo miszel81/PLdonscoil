6da00803e501f191b7f12c2f728e9e69a7335cfb
Marcin Stepien <ms@mmlab.ie>
2020-02-21 18:09:33 +0000

    dev.js

---
+++ src/config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apiUrl: "http://localhost:3400/api",
+  apiAuth: "http://localhost:3400/api/auth",
+  apiDashboard: "http://localhost:3400/api/accounts/myprofile",
+  apiAccount: "http://localhost:3400/api/accounts/",
+  apiUser: "http://localhost:3400/api/users/",
+  apiProjects: "http://localhost:3400/api/projects/",
+  apiCampaigns: "http://localhost:3400/api/campaigns/",
+  apiAccountProjects: "http://localhost:3400/api/projects/account/",
+  apiHand: "http://localhost:3400/api/hand/"
+};
